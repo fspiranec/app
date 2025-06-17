cd9040e81f57499b789ebe4507dde8765c4285c7
fspiranec <85845602+fspiranec@users.noreply.github.com>
2025-06-17 22:22:13 +0200

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,1 +1,22 @@
+import { useEffect, useState } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../firebase';
+import { Navigate } from 'react-router-dom';
 
+export default function ProtectedRoute({ children }) {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (u) => {
+      setUser(u);
+      setLoading(false);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (!user) return <Navigate to="/" />;
+
+  return children;
+}
